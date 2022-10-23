704c193e93f9d8de9138dc6ba451e888d3e022ed
Aly khaled <101535882+alykhaled22@users.noreply.github.com>
2022-10-23 23:02:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 class BigDecimalInt
 {
+
 private:
+
     char sign;
     string number;
     void fillzeros(string& number1, string& number2);
+
 public:
+
     BigDecimalInt(string digits);
     BigDecimalInt(long long int = 0);
     BigDecimalInt operator+ (BigDecimalInt num2);
@@ -16,19 +20,25 @@ public:
     bool operator< (BigDecimalInt anotherDec);
     bool operator== (BigDecimalInt anotherDec);
     BigDecimalInt operator= (BigDecimalInt anotherDec);
+    BigDecimalInt operator-(BigDecimalInt anotherDec);
     char Sign();
     int Size();
     friend ostream& operator<< (ostream& COUT, BigDecimalInt INT);
+
 };
+
 int BigDecimalInt::Size()
 {
     return (int)number.size();
 }
+
 ostream& operator<< (ostream& COUT, BigDecimalInt INT)
 {
     COUT << (INT.sign == '-' ? "-" : "") << INT.number;
     return COUT;
+
 }
+
 bool BigDecimalInt::operator< (BigDecimalInt anotherDec)
 {
     string first = this->number;
@@ -52,13 +62,18 @@ bool BigDecimalInt::operator< (BigDecimalInt anotherDec)
         }
         return false;
     }
+
 }
+
+
 BigDecimalInt BigDecimalInt::operator= (BigDecimalInt anotherDec)
 {
     this->number = anotherDec.number;
     this->sign = anotherDec.sign;
     return *this;
 }
+
+
 bool BigDecimalInt::operator== (BigDecimalInt anotherDec)
 {
     string first = this->number;
@@ -74,6 +89,8 @@ bool BigDecimalInt::operator== (BigDecimalInt anotherDec)
         return true;
     }
 }
+
+
 bool BigDecimalInt::operator>(BigDecimalInt anotherDec)
 {
     string first = this->number;
@@ -89,7 +106,7 @@ bool BigDecimalInt::operator>(BigDecimalInt anotherDec)
         }
         return false;
     }
-    else {   
+    else {
         for (int i = 0; i < first.length(); i++)
         {
             if (first[i] > second[i]) return false;
@@ -97,11 +114,16 @@ bool BigDecimalInt::operator>(BigDecimalInt anotherDec)
         }
         return false;
     }
+
 }
+
+
 char BigDecimalInt::Sign()
 {
     return sign;
 }
+
+
 BigDecimalInt::BigDecimalInt(string digits)
 {
     regex num("[+,-]?[0-9]+");
@@ -123,6 +145,8 @@ BigDecimalInt::BigDecimalInt(string digits)
         cout << "Invalid Input!" << endl;
     }
 }
+
+
 BigDecimalInt::BigDecimalInt(long long int num)
 {
     string digits = to_string(num);
@@ -137,44 +161,123 @@ BigDecimalInt::BigDecimalInt(long long int num)
         number = digits;
     }
 }
+
+
 BigDecimalInt BigDecimalInt::operator+ (BigDecimalInt num2)
 {
-    string result;
-    string first = number;
-    string second = num2.number;
-    fillzeros(first, second);
-    int tm;
-    int carry = 0;
-    for (int i = first.size() - 1; i >= 0; i--)
-    {
-        tm = (first[i] - '0') + (second[i] - '0');
-        tm = (first[i] - '0') + (second[i] - '0') + carry;
-        if (tm > 9)
-        {
-            tm -= 10;
-            carry = 1;
-        }
-        else
-            carry = 0;
+    if (sign == '-' && num2.sign == '+') {
+        sign = '+';
+        return(num2 - *this);
 
-        result = char(tm + '0') + result;
     }
-    if (carry)
-        result = "1" + result;
-    if (sign == '-' && num2.sign == '-')
-        result = "-" + result;
-    BigDecimalInt sum(result);
-    return sum;
+    else if (sign == '+' && num2.sign == '-') {
+        num2.sign = '+';
+        return(*this - num2);
+
+    }
+    else {
+        string result;
+        string first = number;
+        string second = num2.number;
+        fillzeros(first, second);
+        int tm;
+        int carry = 0;
+        for (int i = first.size() - 1; i >= 0; i--)
+        {
+            tm = (first[i] - '0') + (second[i] - '0');
+            tm = (first[i] - '0') + (second[i] - '0') + carry;
+            if (tm > 9)
+            {
+                tm -= 10;
+                carry = 1;
+            }
+            else
+                carry = 0;
+
+            result = char(tm + '0') + result;
+        }
+        if (carry)
+            result = "1" + result;
+        if (sign == '-' && num2.sign == '-')
+            result = "-" + result;
+        BigDecimalInt sum(result);
+        return sum;
+    }
 }
+
+
 void BigDecimalInt::fillzeros(string& number1, string& number2)
 {
     while (number1.size() > number2.size()) number2.insert(0, 1, '0');
     while (number1.size() < number2.size()) number1.insert(0, 1, '0');
 }
 
+
+
+BigDecimalInt BigDecimalInt::operator-(BigDecimalInt anotherDec) {
+
+
+    if (sign == '+' && anotherDec.sign == '-') {
+        anotherDec.sign = '+';
+        return(*this + anotherDec);
+    }
+    else if (sign == '-' && anotherDec.sign == '-') {
+        anotherDec.sign = '+';
+        sign = '+';
+        return(anotherDec - *this);
+    }
+
+    else if (sign == '-' && anotherDec.sign == '+') {
+        anotherDec.sign = '-';
+        return(*this + anotherDec);
+    }
+
+    else {
+        string big = number, small = anotherDec.number;
+        int n = 0, flow = 0;
+        string newNum = "";
+        char s;
+        fillzeros(small, big);
+
+        if (*this < anotherDec) {
+            swap(small, big);
+            s = '-';
+        }
+
+        for (int i = big.size(); i > 0; i--)
+        {
+            n = (big[i - 1] - 48) - (small[i - 1] - 48) - flow;
+            flow = 0;
+            if (n < 0) {
+
+                n += 10;
+                flow = 1;
+
+            }
+            newNum += char(n + 48);
+
+        }
+
+        reverse(newNum.begin(), newNum.end());
+
+        while (newNum[0] == '0' && newNum.size() > 1) newNum = newNum.erase(0, 1);
+
+
+        if (*this < anotherDec) {
+            newNum = s + newNum;
+        }
+        BigDecimalInt num3(newNum);
+        return num3;
+
+    }
+
+}
+
+
 int main()
 {
-    BigDecimalInt hah("+194445");
-    BigDecimalInt hah1("+194446");
-    cout << (hah < hah1) << endl;
+    BigDecimalInt hah("-100");
+    BigDecimalInt hah1("2");
+    BigDecimalInt hah3 = hah - hah1;
+    cout << hah3 << endl;
 }
